5dfdb2a2bd805e506bd7328288fd0ebb037f158c
WKM <wmacura@gmail.com>
2024-12-22 21:12:25 -0500

    d2p2

--- aoc2024/lib/day2.ml
+++ aoc2024/lib/day2.ml
@@ -9,7 +9,7 @@ let map_consecutive ~f elements =
   iter [] elements
 ;;
 
-let is_safe record =
+let is_safe_already record =
   let deltas = map_consecutive ~f:(fun a b -> b - a) record in
   let safe_negative = List.for_all deltas ~f:(fun d -> -3 <= d && d <= -1) in
   let safe_positive = List.for_all deltas ~f:(fun d -> 1 <= d && d <= 3) in
@@ -25,8 +25,10 @@ let sample_data = {|
 1 3 6 7 9
 |}
 
-let%expect_test _ =
-  let safeties = Util.read_string sample_data |> List.map ~f:(fun r -> is_safe r) in
+let%expect_test "part 1 sample" =
+  let safeties =
+    Util.read_string sample_data |> List.map ~f:(fun r -> is_safe_already r)
+  in
   List.iter safeties ~f:(fun s -> print_endline (Bool.to_string s));
   [%expect {|
     true
@@ -42,12 +44,37 @@ let%expect_test _ =
     |}]
 ;;
 
-let%expect_test "part 1" =
+let%expect_test "part 1 submittal" =
   let data = Util.read_integer_file "/Users/wkm/Code/aoc/aoc2024/data/day2" in
-  List.count data ~f:is_safe |> printf "%d\n";
+  List.count data ~f:is_safe_already |> printf "%d\n";
   [%expect {| 686 |}]
 ;;
 
-(* Part 2 *)
-(* If a system is unsafe, attempt to drop the one value (add the next delta and see if it's
-   sufficient) *)
+let without l n = List.filteri l ~f:(fun i _ -> i <> n)
+
+let could_be_safe record =
+  is_safe_already record
+  || (* attempt to drop each element and then check if the record is safe *)
+  List.existsi record ~f:(fun i _ -> without record i |> is_safe_already)
+;;
+
+let%expect_test "part 2 sample" =
+  let safeties = Util.read_string sample_data |> List.map ~f:(fun r -> could_be_safe r) in
+  List.iter safeties ~f:(fun s -> print_endline (Bool.to_string s));
+  [%expect {|
+    true
+    false
+    false
+    true
+    true
+    true
+    |}];
+  List.count safeties ~f:Fn.id |> printf "%d\n";
+  [%expect {| 4 |}]
+;;
+
+let%expect_test "part 2 submittal" =
+  let data = Util.read_integer_file "/Users/wkm/Code/aoc/aoc2024/data/day2" in
+  List.count data ~f:could_be_safe |> printf "%d\n";
+  [%expect {| 717 |}]
+;;
