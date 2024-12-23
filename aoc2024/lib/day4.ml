8c459730913d2e19961482c635191e08f0c7be7f
WKM <wmacura@gmail.com>
2024-12-23 09:38:11 -0500

    d4p2

--- aoc2024/lib/day4.ml
+++ aoc2024/lib/day4.ml
@@ -33,8 +33,7 @@ let search_direction grid x y dx dy =
 ;;
 
 let search_spot grid x y =
-  List.fold directions ~init:0 ~f:(fun acc (dx, dy) ->
-    if search_direction grid x y dx dy then acc + 1 else acc)
+  List.count directions ~f:(fun (dx, dy) -> search_direction grid x y dx dy)
 ;;
 
 let search grid =
@@ -53,4 +52,44 @@ let%expect_test "part 1 submittal" =
   |> search
   |> printf "%d\n";
   [%expect {| 2639 |}]
+;;
+
+let p2_term = "MAS" |> String.to_array
+let p2_diagonals = [ 1, 1; -1, -1; 1, -1; -1, 1 ]
+
+let p2_search_direction grid x y dx dy =
+  Array.for_alli p2_term ~f:(fun i c ->
+    let x = x + (i * dx)
+    and y = y + (i * dy) in
+    if x < 0 || y < 0 || x >= Array.length grid || y >= Array.length grid.(0)
+    then false
+    else Char.equal c grid.(x).(y))
+;;
+
+let p2_search_cell grid x y =
+  2
+  = List.count p2_diagonals ~f:(fun (dx, dy) ->
+    let x = x - dx
+    and y = y - dy in
+    p2_search_direction grid x y dx dy)
+;;
+
+let p2_search grid =
+  Array.foldi grid ~init:0 ~f:(fun x acc row ->
+    acc
+    + Array.foldi row ~init:0 ~f:(fun y acc _ ->
+      acc + (p2_search_cell grid x y |> Bool.to_int)))
+;;
+
+let%expect_test "part 2 sample" =
+  sample_data |> array_of_data |> p2_search |> printf "%d\n";
+  [%expect {| 9 |}]
+;;
+
+let%expect_test "part 2 submittal" =
+  In_channel.read_all "/Users/wkm/Code/aoc/aoc2024/data/day4"
+  |> array_of_data
+  |> p2_search
+  |> printf "%d\n";
+  [%expect {| 2005 |}]
 ;;
